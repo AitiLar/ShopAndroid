1f6ffbf831ef4a1582b9800f84532655d60dd35c
AitiLar <nparada@bk.ru>
2021-09-16 16:54:18 +0300

    first commit

---
+++ app/src/main/java/com/google/shops/AdminCategoryActivity.java
@@ -0,0 +1,98 @@
+package com.google.shops;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+    private ImageView shtork, shtorf, shtorr, shtora;
+    private ImageView glasses, hatsCaps, walletsBagsPurses, shoes;
+    private ImageView headPhonesHandFree, Laptops, watches, mobilePhones;
+    private Button LogoutBtn, CheckOrdersBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_btn);
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        CheckOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
+
+
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this, AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        shtork = (ImageView) findViewById(R.id.t_shirts);
+        shtorf = (ImageView) findViewById(R.id.sports_t_shirts);
+        shtorr = (ImageView) findViewById(R.id.female_dresses);
+        shtora = (ImageView) findViewById(R.id.sweathers);
+
+
+        shtork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Шторы Кафе");
+                startActivity(intent);
+            }
+        });
+        shtorf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Французские шторы");
+                startActivity(intent);
+            }
+        });
+
+
+        shtorr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Римские шторы");
+                startActivity(intent);
+            }
+        });
+
+
+        shtora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Австрийские шторы");
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/google/shops/Prevalent.java
@@ -0,0 +1,8 @@
+package com.google.shops;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
